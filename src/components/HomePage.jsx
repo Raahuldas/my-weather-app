ed766707ca7dd1e63a15cae79ce6444b4926b23a
raahuldas <rahuldas42coc@gmail.com>
2024-04-14 20:51:19 +0530

    bug fix

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -49,14 +49,12 @@ function HomePage() {
   };
 
   useEffect(() => {
-    
     let searched = "";
     if (search) {
       searched = '&where=search(name,"' + search + '")';
     } else {
       searched = "";
     }
-
     fetch(
       `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=20&order_by=${orderBy} ${sortBy}&offset=${offset}${searched}`
     )
